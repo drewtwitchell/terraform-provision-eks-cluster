e2f6ee9a020c2b25760a7964451d7dfd39734768
Drew Twitchell <19315286+drewtwitchell@users.noreply.github.com>
2025-02-27 11:13:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "eks" {
   }
 
   vpc_id     = data.aws_vpc.existing.id
-  subnet_ids = data.aws_subnets.private.ids # Use private subnets for worker nodes
+  subnet_ids = data.aws_subnets.private.ids  # Use private subnets for worker nodes
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
@@ -112,7 +112,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Public access
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -120,7 +120,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Public access
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -183,10 +183,27 @@ resource "aws_lb_listener" "http_listener" {
   }
 }
 
-# Register EKS nodes in the Target Group
+# ---------------------------------------------------------------
+# Lookup the EC2 instances for the node group "one" using tags
+# ---------------------------------------------------------------
+data "aws_instances" "eks_node_group_one" {
+  filter {
+    name   = "tag:eks:nodegroup-name"
+    values = ["node-group-1"]
+  }
+
+  # Ensure only instances in the desired VPC are returned
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
+
+# Register EKS nodes from node group "one" in the Target Group
 resource "aws_lb_target_group_attachment" "eks_nodes" {
-  count            = length(module.eks.eks_managed_node_groups["one"].instances)
+  count            = length(data.aws_instances.eks_node_group_one.ids)
   target_group_arn = aws_lb_target_group.eks_target_group.arn
-  target_id        = module.eks.eks_managed_node_groups["one"].instances[count.index].id
+  target_id        = data.aws_instances.eks_node_group_one.ids[count.index]
   port             = 80
 }
+
